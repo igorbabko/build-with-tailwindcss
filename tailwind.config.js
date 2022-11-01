116aae9775e2ce533463011a240f1ffaa4651b08
Igor Babko <i.i.babko@gmail.com>
2022-11-01 12:19:01 +0200

    container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["*.html"],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+      'lg': '1024px',
+    },
+    container: {
+      center: true,
+      padding: '1.5rem',
+    },
     extend: {},
   },
   plugins: [
